39ac48331fea83247267c1a5d519d07ce7630568
MarkAnthony <markanthonylaredo@gmail.com>
2022-12-28 21:21:33 +0800

    Added parameter ReviewQueryDto that accept user input and sorting

--- Services/IYelpService.cs
+++ Services/IYelpService.cs
@@ -2,6 +2,11 @@
 {
     public interface IYelpService
     {
-        Task<IEnumerable<YelpReview>> GetReviewsAsync();
+        /// <summary>
+        /// Retrieves reviews from the Yelp API using the specified query parameters.
+        /// </summary>
+        /// <param name="query">The query parameters to use for filtering and sorting the reviews.</param>
+        /// <returns>A collection of <see cref="YelpReview"/> objects representing the reviews retrieved from the Yelp API.</returns>
+        Task<IEnumerable<YelpReview>> GetReviewsAsync(ReviewQueryDto query);
     }
 }
---
+++ Services/ReviewQueryDto.cs
@@ -0,0 +1,18 @@
+﻿public class ReviewQueryDto
+{
+    /// <summary>
+    /// The current page of the review data to retrieve. Default is 1.
+    /// </summary>
+    public int Offset { get; set; } = 1;
+
+    /// <summary>
+    /// The number of reviews to retrieve per page. Default is 20.
+    /// </summary>
+    public int Limit { get; set; } = 20;
+
+    /// <summary>
+    /// The criterion to use for sorting the reviews by "yelp_sort" and "newest". Default is "yelp_sort".
+    /// </summary>
+    public string SortBy { get; set; } = "yelp_sort";
+
+}
