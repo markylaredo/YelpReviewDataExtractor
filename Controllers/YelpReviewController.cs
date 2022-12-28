d72c7b321897f12824f075b9bf49b5fc829d07a1
MarkAnthony <markanthonylaredo@gmail.com>
2022-12-28 15:31:19 +0800

    Added Yelp review controller

---
+++ Controllers/YelpReviewController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YelpReviewDataExtractor.Services;
+
+namespace YelpReviewDataExtractor.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class YelpReviewController : ControllerBase
+    {
+        private readonly IYelpService _yelpService;
+
+        public YelpReviewController(IYelpService yelpService)
+        {
+            _yelpService = yelpService;
+        }
+       
+
+
+        [HttpGet]
+        public async Task<IActionResult> Reviews()
+        {
+            var reviews = await _yelpService.GetReviewsAsync();
+
+            return Ok(reviews);
+        }
+    }
+}
